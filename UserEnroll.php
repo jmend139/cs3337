06f8a2cca68bf9a94a1db8973f89763738231306
Jonathan Mendoza <43479012+jmend139@users.noreply.github.com>
2019-03-08 19:24:31 -0800

    Update UserEnroll.php

--- UserEnroll.php
+++ UserEnroll.php
@@ -1,39 +1,34 @@
-<<!DOCTYPE html>
+<!doctype html>
 <html>
 <head>
-    <meta charset="utf-8">
-
-    <title>User Enroll Page</title>
-
+<meta charset="utf-8">
+<title>User Enroll Page</title>
 </head>
 <body>
-    <p>User Enroll Page</p>
+<p align="center"> User Enroll Page</p>
 <form action="UserInsert.php" method="post">
 <table align="center">
-<tr>
-<td>
-User Name
-</td>
-<td>
-<input type="text" name="name" />
-</td>
-</tr>
-<tr>
-<td>
-Email
-</td>
-<td>
-<input type="text" name="email" />
-</td>
-</tr>
-<tr>
-<td>
-<input type="submit" value="Submit" />
-</td>
-</tr>
+	<tr>
+		<td>
+			User Name
+		</td>
+		<td>
+			<input type="text" name="name" />
+		</td>
+	</tr>
+	<tr>
+		<td>
+			Email
+		</td>
+		<td>
+			<input type="text" name="email" />
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<input type="submit" value="submit" />
+		</td>
+	</tr>
 </form>
-</body>
-
-
 </body>
 </html>
